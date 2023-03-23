a72c09a08c9eee892e0d1b5c0b4ac2e56458a1cb
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2023-03-22 21:44:06 -0300

    created packages and models

---
+++ src/main/java/com/alunoonline/api/service/AlunoService.java
@@ -0,0 +1,14 @@
+package com.alunoonline.api.service;
+
+import com.alunoonline.api.model.Aluno;
+import com.alunoonline.api.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class AlunoService {
+
+    @Autowired
+    AlunoRepository repository;
+    public Aluno create(Aluno aluno){
+        return repository.save(aluno);
+    }
+}
