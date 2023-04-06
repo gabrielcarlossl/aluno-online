9aa7560f625ca3216425d32099012abd611227be
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2023-04-05 21:30:06 -0300

    added Service annotation

--- src/main/java/com/alunoonline/api/service/AlunoService.java
+++ src/main/java/com/alunoonline/api/service/AlunoService.java
@@ -3,7 +3,9 @@ package com.alunoonline.api.service;
 import com.alunoonline.api.model.Aluno;
 import com.alunoonline.api.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AlunoService {
 
     @Autowired
@@ -11,4 +13,5 @@ public class AlunoService {
     public Aluno create(Aluno aluno){
         return repository.save(aluno);
     }
+
 }
