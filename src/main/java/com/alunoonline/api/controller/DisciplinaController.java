fe85fdfbd712205bb663bc957268f6a905e09f9f
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2023-05-03 22:22:59 -0300

    added patch method to disciplina

--- src/main/java/com/alunoonline/api/controller/DisciplinaController.java
+++ src/main/java/com/alunoonline/api/controller/DisciplinaController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/disciplina")
 public class DisciplinaController {
@@ -20,5 +22,20 @@ public class DisciplinaController {
     public ResponseEntity<Disciplina> create(@RequestBody Disciplina disciplina){
         Disciplina disciplinaCreated = service.create(disciplina);
         return ResponseEntity.status(201).body(disciplinaCreated);
+    }
+
+    @PatchMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Disciplina> update(@PathVariable Long id, @RequestBody Disciplina disciplinaUpdate){
+        Optional<Disciplina> optionalDisciplina = service.findById(id);
+        if (optionalDisciplina.isPresent()){
+            Disciplina disciplina = optionalDisciplina.get();
+            disciplina.setNome(disciplinaUpdate.getNome());
+            disciplina.setProfessor(disciplinaUpdate.getProfessor());
+            service.save(disciplina);
+            return ResponseEntity.ok(disciplina);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/com/alunoonline/api/service/DisciplinaService.java
+++ src/main/java/com/alunoonline/api/service/DisciplinaService.java
@@ -6,6 +6,8 @@ import com.alunoonline.api.repository.DisciplinaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class DisciplinaService {
 
@@ -15,4 +17,6 @@ public class DisciplinaService {
     public Disciplina create(Disciplina disciplina){
         return repository.save(disciplina);
     }
+    public Optional<Disciplina> findById(Long id) {return repository.findById(id);}
+    public Disciplina save(Disciplina disciplina){ return repository.save(disciplina);}
 }
