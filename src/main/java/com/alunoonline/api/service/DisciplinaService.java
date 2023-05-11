96a325bcf41e9bc8bfdbcbccc54138a6be63ec08
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2023-05-10 21:57:15 -0300

    added find all and find by professor id

--- src/main/java/com/alunoonline/api/service/DisciplinaService.java
+++ src/main/java/com/alunoonline/api/service/DisciplinaService.java
@@ -6,6 +6,7 @@ import com.alunoonline.api.repository.DisciplinaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,6 +18,11 @@ public class DisciplinaService {
     public Disciplina create(Disciplina disciplina){
         return repository.save(disciplina);
     }
+    public List<Disciplina> findAll(){ return repository.findAll(); }
     public Optional<Disciplina> findById(Long id) {return repository.findById(id);}
     public Disciplina save(Disciplina disciplina){ return repository.save(disciplina);}
+
+    public List<Disciplina> findByProfessorId(Long professorId){
+        return repository.findByProfessorId(professorId);
+    }
 }
