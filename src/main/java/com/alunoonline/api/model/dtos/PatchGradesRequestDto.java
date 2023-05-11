74b944308f88db3b30fdaabb0e2eecb4490ac2ce
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2023-05-10 21:51:38 -0300

    created class to update grades

--- src/main/java/com/alunoonline/api/model/dtos/PatchGradesRequestDto.java
+++ src/main/java/com/alunoonline/api/model/dtos/PatchGradesRequestDto.java
@@ -1,2 +1,14 @@
-package com.alunoonline.api.model.dtos;public class PatchGradesRequestDto {
+package com.alunoonline.api.model.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PatchGradesRequestDto {
+    private Double nota1;
+    private Double nota2;
+
 }
